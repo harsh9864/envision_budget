79f37d1f6006ed52dabad8f180648e82a876506d
DhruvilMistry9800 <dhruvil@sanskartechnolab.com>
2024-08-28 19:02:57 +0530

    Testing and modifying the Item wise budget against PO

--- envision_finance/envision_finance/doctype/item_wise_budget/item_wise_budget.js
+++ envision_finance/envision_finance/doctype/item_wise_budget/item_wise_budget.js
@@ -1,8 +1,92 @@
-// Copyright (c) 2024, Dhruvil Mistry and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Item wise Budget", {
 
-// frappe.ui.form.on("Item wise Budget", {
-// 	refresh(frm) {
+    quotation: function(frm) {
+        frappe.call({
+            method: "frappe.client.get",
+            args: {
+                doctype: "Quotation",
+                name: frm.doc.quotation
+            },
+            callback:function(response){
+                console.log(response.message.items)
+                let row = response.message.items
+                frm.clear_table("budgeted_items");
+                row.forEach(data => {
+                    console.log(data.item_code);
+                    let row = frm.add_child('budgeted_items',{
+                        item:data.item_code,
+                        quantity:data.qty,
+                        unit_price:data.rate,
+                        amount:data.amount,
+                        current_budget:data.amount,
+                        hsn_sac_code:data.gst_hsn_code,
+                        item_group:data.item_group,
+                        uom:data.uom
+                    })
+                    frm.refresh_field("budgeted_items")
+                })
 
-// 	},
-// });
+            }
+        });
+    },
+});
+
+frappe.ui.form.on("Budget Items",{
+
+    
+    quantity:function(frm,cdt,cdn){
+        var row = locals[cdt][cdn];
+        var amount = row.quantity * row.unit_price;
+        if(amount == NaN){
+            frappe.model.set_value(cdt, cdn, 'amount', 0);
+        }
+        else{
+            console.log(amount)
+            frappe.model.set_value(cdt, cdn, 'amount', amount);
+        }
+    },
+
+    unit_price:function(frm,cdt,cdn){
+        var row = locals[cdt][cdn];
+        var amount = row.quantity * row.unit_price;
+        if(amount == NaN){
+            frappe.model.set_value(cdt, cdn, 'amount', 0);
+        }
+        else{
+            console.log(amount)
+            frappe.model.set_value(cdt, cdn, 'amount', amount);
+        }
+
+    },
+    amount:function(frm,cdt,cdn){
+        var row = locals[cdt][cdn];
+        frappe.model.set_value(cdt, cdn, 'current_budget', row.amount);
+    },
+
+    item:function(frm,cdt,cdn){
+        // Get the current row
+        var row = locals[cdt][cdn];
+        // Call the backend method to verify the item
+        frappe.call({
+            // Method to call
+            method: "envision_finance.envision_finance.doctype.item_wise_budget.item_wise_budget.verifying_the_budgeted_items",
+            // Arguments to pass
+            args:{
+                project: frm.doc.project, // Project from the current form
+                department: frm.doc.department, // Department from the current form
+                item:row.item // Item from the current row
+            },
+            // Callback function to handle the response
+            callback:function(response){
+                // If no data is returned, log a message
+                if(response.data == null){
+                    console.log("...")
+                }
+                // If data is returned, throw an error with the budget name
+                else{
+                    frappe.throw(`<b>Item Already exists in another Budget</b><br><br><b>${row.item}</b> already exists in Budget: <b>${response.data[0].name}</b>`)
+                }
+            }
+        })
+    }
+})
--- envision_finance/envision_finance/doctype/item_wise_budget/item_wise_budget.py
+++ envision_finance/envision_finance/doctype/item_wise_budget/item_wise_budget.py
@@ -1,9 +1,39 @@
 # Copyright (c) 2024, Dhruvil Mistry and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from typing import Union,List,Dict,Any
 from frappe.model.document import Document
 
 
 class ItemwiseBudget(Document):
 	pass
+
+@frappe.whitelist()
+def verifying_the_budgeted_items() -> Union[List[Dict[str, Any]],None]:
+	project:str = frappe.form_dict['project']
+	department:str = frappe.form_dict['department']
+	item:str = frappe.form_dict['item']
+
+	# BI -> Budget Items
+	# IB -> Item wise Budget
+
+	item_data_sql:str = f"""
+	
+	SELECT 
+	BI.item,
+	IB.name
+	FROM `tabBudget Items` AS BI
+	INNER JOIN `tabItem wise Budget` AS IB ON IB.name = BI.parent
+	WHERE IB.project = "{project}"
+	AND IB.department = "{department}"
+	AND BI.item = "{item}"
+	AND IB.docstatus = 1
+	
+	"""
+	
+	item_data:List[Dict[str, Any]] = frappe.db.sql(item_data_sql, as_dict=True)
+	if item_data:
+		frappe.response['data'] = item_data
+	else:
+		frappe.response['data'] = None
